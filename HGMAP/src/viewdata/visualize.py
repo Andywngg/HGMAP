ad4e85b55a1b03bad715a3dfbe57352bac04490d
Andy Wang <andywang.437@gmail.com>
2025-01-21 12:51:48 -0500

    Inital Commit

--- HGMAP/src/viewdata/visualize.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Visualize feature importance
-importances = stacked_model.named_estimators_['rf'].feature_importances_
-sns.barplot(x=importances, y=data.columns[:-1])  # Adjust column indexing
-plt.title("Feature Importances")
-plt.show()
